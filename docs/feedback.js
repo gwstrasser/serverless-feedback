21407cafbd41f1b47738e570e0ebbf06cde3643b
gwstrasser <60398777+gwstrasser@users.noreply.github.com>
2020-01-31 10:50:48 +0100

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://4f6301ee.eu-gb.apigw.appdomain.cloud/7-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
